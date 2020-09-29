6e7baec3b5f07f31907cd12e4999a5a72a9e969c
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2020-09-29 20:01:59 +0200

    add handlers for multiple users and aliases

--- printcodebot.py
+++ printcodebot.py
@@ -13,15 +13,33 @@ bot = telegram.Bot(TOKEN)
 
 arguments = sys.argv
 code = ""
-USERNAME = ""
+usernames_raw = ""
 
 for i in range (1, len(arguments)):
 	if (arguments [i] == "-u"):
-		USERNAME = arguments[i+1]
+		usernames_raw = arguments[i+1]
 	if (arguments [i] == "-f"):
 		code += arguments[i+1] + " "
 
 code += "> .stuff.txt"
+
+usernames = usernames_raw.split(',')
+f = open('config/users.txt', 'r')
+lines = []
+for line in f:
+	lines.append(line)
+f.close()
+
+usernames_def = []
+
+for username in usernames:
+	if username.isnumeric():
+		usernames_def.append(username)
+	else:
+		for line in lines:
+			if username == line.split(' ')[0]:
+				usernames_def.append(line.split(' ')[1])
+				break
 
 subprocess.call(code, shell=True)
 
@@ -33,4 +51,6 @@ for line in f:
 
 f.close()
 
-bot.send_message(chat_id=USERNAME, text=text2)
+for USERNAME in usernames_def:
+	bot.send_message(chat_id=USERNAME, text=text2)
+
